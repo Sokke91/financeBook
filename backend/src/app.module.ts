e914bf8b6e2b35e541bd3e588fa5f74449d7eb9f
Sokke91 <jonas.angel.ja@gmail.com>
2022-11-17 14:40:34 +0100

    scaffold contrac resource`

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContractModule } from './contract/contract.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'financeBook',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ContractModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
